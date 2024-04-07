37f58b025914a9b9bd1814ae8fc6c07b5ad7f6cf
Ausaf Liaquat <ausaf.wpt@gmail.com>
2024-04-07 23:05:34 +0500

    search filtered ui added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar  from "@/components/Navbar";
+import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
@@ -16,8 +16,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="relative">
-      <Navbar />
-        {children}
+        <Navbar />
+          {children}
         <Footer />
       </body>
     </html>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -30,7 +30,6 @@ const Hero = () => {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png" alt="Hero" fill className="object-contain" />
-          
         </div>
         <div className="hero__image-overlay" />
       </div>
